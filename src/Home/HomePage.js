9a5886fe56ed44477955db50637ef9337401eb47
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2022-09-02 12:25:03 +0100

    Landing page completed

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import BigCard from '../components/BigCard';
 import Button from '../components/Button';
+import Card from '../components/Card';
 import Input from '../components/Input';
 import Navbar from '../components/Navbar';
 import './homepage.css';
@@ -17,16 +19,104 @@ function HomePage() {
 			</header>
 			<section className='event-section'>
 				<form className='search-form'>
-					<div className='form-input'>
+					<div className='search-input'>
 						<Input placeholder='Search Event' />
 					</div>
-					<div className='form-input'>
+					<div className='category-input'>
 						<Input placeholder='Categories' />
 					</div>
 					<div className='form-btn'>
 						<Button title='Search Event' variation={'secondary'} />
 					</div>
 				</form>
+
+				<div className='event-container'>
+					<p className='section-title'>Events near you</p>
+					<div className='events'>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+						<Card
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							date='10/10/2022'
+						/>
+					</div>
+				</div>
+				<div className='event-container'>
+					<p className='section-title'>Featured Events</p>
+					<div className='featured-section'>
+						<BigCard
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							discount='20% off'
+						/>
+						<BigCard
+							name='The Concert'
+							image={'/image/image.png'}
+							location='Lagos'
+							price='N2000'
+							discount='20% off'
+						/>
+					</div>
+				</div>
 			</section>
 		</>
 	);
