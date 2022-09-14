a9a2867fcd843d4688dd0a732d1e0037daf29172
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2022-09-14 12:57:54 +0100

    Finished payment previe page

--- src/Event/components/Content.js
+++ src/Event/components/Content.js
@@ -1,35 +1,20 @@
 import './content.css';
 import React from 'react';
 import TicketCategory from './TicketCategory';
+import EventInfo from './EventInfo';
 
 function Content({ setModal }) {
 	return (
 		<section className='main-container'>
-			<div className='event-content-container'>
-				<div className='event-info'>
-					<p className='info-title'>Event Description</p>
-					<p className='info-description'>
-						The play, "Pelumi," is a musical love story that revolves around
+			<EventInfo
+				eventDescription={`The play, "Pelumi," is a musical love story that revolves around
 						Pelumi, a woman who was in a relationship with Adigun, a man with
 						direction and purpose, but felt pressured by her mother to find love
 						with someone else because of her mother's wealth and availability.
-						You won’t want to miss this show on this year valentine.
-					</p>
-				</div>
-				<div className='event-info'>
-					<p className='info-title'>Date and Time</p>
-					<div className='info-description'>
-						<p className='date'>14th February, 2022</p>
-						<p className='time'>5PM Prompt</p>
-					</div>
-				</div>
-				<div className='event-info'>
-					<p className='info-title'>Venue</p>
-					<p className='info-description'>
-						Oduduwa Hall, Obafemi Awolowo University, Ile-Ife, Osun State
-					</p>
-				</div>
-			</div>
+						You won’t want to miss this show on this year valentine.`}
+				dateTime={'14th February, 2022'}
+				eventVenue='Oduduwa Hall, Obafemi Awolowo University, Ile-Ife, Osun State'
+			/>
 
 			<div className='ticket-container'>
 				<p className='ticket-title'>Get your Ticket</p>
