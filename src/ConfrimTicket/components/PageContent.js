6b023c7f146431b22c86083bef3d639a56af8640
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2022-09-19 09:56:09 +0100

    fixed the responsiveness of recipt

--- src/ConfrimTicket/components/PageContent.js
+++ src/ConfrimTicket/components/PageContent.js
@@ -6,27 +6,36 @@ import PurchaseSummary from './PurchaseSummary';
 import EventInfo from './../../Event/components/EventInfo';
 import OrganizerDescription from '../../components/OrganizerDescription';
 import TicketDetails from './TicketDetails';
-import ConfirmModal from './ConfirmModal';
+// import ConfirmModal from './ConfirmModal';
 
 function PageContent() {
 	const ref = React.createRef();
+	const btnref = React.createRef();
+
+	function handlePdfComplete() {
+		ref.current.style.width = '100%';
+		btnref.current.style.display = 'flex';
+	}
 	return (
 		<div ref={ref} className='page-content-container'>
 			<TicketDetails />
 			<PurchaseSummary titleState={false} />
-			<div className='ticket-action-btn'>
-				<ReactToPdf targetRef={ref} filename='recipt.pdf'>
+			<div ref={btnref} className='ticket-action-btn'>
+				<ReactToPdf targetRef={ref} onComplete={handlePdfComplete}>
 					{({ toPdf }) => (
 						<div>
 							<Button
-								onclick={toPdf}
+								onclick={() => {
+									ref.current.style.width = '800px';
+									btnref.current.style.display = 'none';
+									toPdf();
+								}}
 								variation={'secondary'}
 								title='Download ticket'
 							/>
 						</div>
 					)}
 				</ReactToPdf>
-
 				<div>
 					<Button variation={'primary'} title='Share Event' />
 				</div>
